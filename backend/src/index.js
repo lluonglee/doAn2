5fa08dd64cb9fcd761abf6934d415746baf22749
lluonglee <lluonglee14082002@gmail.com>
2024-09-30 11:20:48 +0700

    setup datadase and server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser");
 
-const app = express()
-const PORT = 5000;
- 
-app.get("/", (req, res) =>{
-    res.send("Hello world");
+
+dotenv.config()
+const app = express();
+
+mongoose.connect(process.env.MONGODB_CONNECT)
+.then(() =>{
+    console.log("Connect to database")
 })
+.catch((error) =>{
+    console.log(error);
+})
+app.use(cors());
+app.use(cookieParser())
+app.use(express.json())
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}!!`)
+app.listen(5000, () =>{
+    console.log("Server is running!!")
 })
---
+++ backend/src/routes/index.js
@@ -0,0 +1,7 @@
+const userRouter = require("./userRouter")
+
+const routes = (app) =>{
+    app.use('/api/user',)
+}
+
+module.exports = routes
